09d5e085ea8821c7bb06824cd216fa730be40b4f
ruby.wilde@pm.me <44.5.53.6.k@gmail.com>
2025-01-01 17:12:41 +0100

    Updated project configuration

--- oh-my-cheese/eslint.config.mjs
+++ oh-my-cheese/eslint.config.mjs
@@ -1,15 +1,29 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.js", "**/*.jsx"],
+    ...reactPlugin.configs.flat.recommended, // flat configs
+    settings: {
+      react: {
+        version: "detect", // for some reason this is not default, but it is essential to figure out which version of react to use
+      },
+    },
+  },
+  reactPlugin.configs.flat("jsx-runtime"), // this allows skipping importing react in every single file
+  {
+    files: ["**/*.js", "**/*.ts", "**/*.jsx", "**/*.tsx"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
+    },
+    rules: {
+      "react/no-unescaped-entities": "off", // ' $apos
+      "reac/prop-types": "off",
     },
   },
   prettier,
